7900ff6204f4bd70e11e76dc606ef0bc1b4cd80e
louis2688 <louismadrigal26@gmail.com>
2022-03-25 14:09:13 +0800

    Remove userRole and userSite entity

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -10,15 +10,12 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { Role } from './enums/role.enum';
-import { Roles } from './roles.decorator';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly usersService: UserService) {}
 
   @Post()
-  @Roles(Role.ADMIN)
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
@@ -34,13 +31,11 @@ export class UserController {
   }
 
   @Patch(':id')
-  @Roles(Role.ADMIN)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(+id, updateUserDto);
   }
 
   @Delete(':id')
-  @Roles(Role.ADMIN)
   remove(@Param('id') id: string) {
     return this.usersService.remove(+id);
   }
